d7881624b11bd827b37681ded458f7b081393200
Mbali <mbalikhumalo408@gmail.com>
2023-11-24 11:45:13 +0200

    devops

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const TextInput = () => {
+  const [text, setText] = useState('');
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="textInput">Enter Text: </label>
+      <input
+        type="text"
+        id="textInput"
+        value={text}
+        onChange={handleInputChange}
+        placeholder="Type here..."
+      />
+      <p>Entered Text: {text}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default TextInput;
